99a2a68d595afc32c5a558282837b93180ab7fcd
Celebdil <69753424+Celebdil@users.noreply.github.com>
2022-04-27 23:18:05 +0200

    Add files via upload

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
-import './style.css'
-import { GLTFLoader } from './GLTFLoader_1.js';
+import "./style.css"
+
+import { GLTFLoader } from "./GLTFLoader_1.js";
+import { OrbitControls } from "./OrbitControls_1.js"
 
 import * as THREE from "three";
 
@@ -8,9 +10,10 @@ scene.background = new THREE.Color(0x575757);
 
 const camera = new THREE.PerspectiveCamera(40, window.innerWidth / window.innerHeight, 0.1, 1000);
 camera.position.setX(-2.5);
-camera.position.setY(0.5);
-camera.position.setZ(5);
+camera.position.setY(1);
+camera.position.setZ(3);
 camera.rotateY(-Math.PI / 6);
+camera.rotateX(-Math.PI / 8);
 
 const renderer = new THREE.WebGLRenderer({
   canvas: document.querySelector("#bg"),
@@ -21,16 +24,18 @@ renderer.setPixelRatio(window.devicePixelRatio);
 renderer.setSize(window.innerWidth, window.innerHeight);
 
 
-const hlight = new THREE.AmbientLight(0x404040, 100);
+const hlight = new THREE.AmbientLight(0x404040, 10);
 scene.add(hlight);
 
 
 const loader = new GLTFLoader();
-loader.load("car_new.glb", function(gltf){
+loader.load("old_car_new.glb", function(gltf){
   //car = gltf.scene.children[0];
   //car.scale.set(0.5, 0.5, 0.5);
   scene.add(gltf.scene)
 });
+
+const controls = new OrbitControls(camera, renderer.domElement);
 //const geometry = new THREE.TorusGeometry(10, 3, 16, 100);
 //const material = new THREE.MeshBasicMaterial( {color: 0xfc0a0a} );
 //const mesh = new THREE.Mesh(geometry, material);
@@ -38,6 +43,7 @@ loader.load("car_new.glb", function(gltf){
 
 function animate() {
   requestAnimationFrame(animate);
+  controls.update()
   renderer.render(scene, camera);
 }
 
